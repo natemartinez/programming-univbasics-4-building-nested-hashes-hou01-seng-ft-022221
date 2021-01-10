68d6c532d0b473767d3d0edebbe9490947be89a0
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2021-01-10 23:44:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-monopoly = {:railroads => {:pieces => }}
+monopoly = {:railroads => {:pieces => 4, :rent_in_dollars => {}, :names => {}}}
 end
 
 
